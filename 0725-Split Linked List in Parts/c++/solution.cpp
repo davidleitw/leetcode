1966a79bb720122bea348ff2ef76a8c512122bc2
davidlei <davidleitw@gmail.com>
2020-09-01 00:40:10 +0800

    0725-Split Linked List in Parts better version for c++ solution.

--- 0725-Split Linked List in Parts/c++/solution.cpp
+++ 0725-Split Linked List in Parts/c++/solution.cpp
@@ -69,3 +69,49 @@ public:
 
 // Runtime: 8 ms, faster than 82.42% of C++ online submissions for Split Linked List in Parts.
 // Memory Usage: 8.5 MB, less than 33.07% of C++ online submissions for Split Linked List in Parts.
+
+// version2, a litter butter solution. 
+class Solution {
+public:
+    vector<ListNode*> splitListToParts(ListNode* root, int k) {
+        if(!root) {
+            vector<ListNode*> solution(k, nullptr);
+            return solution;
+        }
+        vector<ListNode*> v;
+        while(root) {
+            v.push_back(root);
+            root = root->next;
+        }
+        int length = v.size();
+        vector<ListNode*> solution;
+        if(length < k) {
+            for(int i = 0; i < length; ++i) {
+                v[i]->next = nullptr;
+                solution.push_back(v[i]);
+            }
+            for(int i = length; i < k; ++i)
+                solution.push_back(nullptr);
+            return solution;
+        } 
+        
+        int index = 0;
+        int exnum = length%k; // 7%3 = 1;
+        int range = length/k; // 7/3 = 2;
+        for(int turn = 0; turn < exnum; ++turn) {
+            solution.push_back(v[index]);
+            v[index+range]->next = nullptr;
+            index = index+range+1;
+        }
+        range--;
+        for(int turn = exnum; turn < k; ++turn) {
+            solution.push_back(v[index]);
+            v[index+range]->next = nullptr;
+            index = index+range+1;
+        }
+        return solution;
+    }
+};
+
+// Runtime: 4 ms, faster than 98.60% of C++ online submissions for Split Linked List in Parts.
+// Memory Usage: 8.5 MB, less than 17.48% of C++ online submissions for Split Linked List in Parts.
