04757ea612becba05fee672309ba4c2509948fdc
davidlei <davidleitw@gmail.com>
2020-10-04 00:18:04 +0800

    0338-Counting Bits golang & c++ solution

--- 0338-Counting Bits/c++/solution.cpp
+++ 0338-Counting Bits/c++/solution.cpp
@@ -13,5 +13,5 @@ public:
     }
 };
 
-// Runtime: 4 ms, faster than 98.50% of C++ online submissions for Counting Bits.
-// Memory Usage: 8.2 MB, less than 43.61% of C++ online submissions for Counting Bits.
+// Runtime: 0 ms, faster than 100.00% of C++ online submissions for Counting Bits.
+// Memory Usage: 8 MB, less than 50.60% of C++ online submissions for Counting Bits.
